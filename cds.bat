8f5877d3e97fefe88dce0dee45ef867ed2a96b67
mmmmm4299 <119816977+mmmmm4299@users.noreply.github.com>
2025-11-15 13:03:09 +0100

    Update cds.bat

--- cds.bat
+++ cds.bat
@@ -1,5 +1,5 @@
 @echo off
 if "%1" == "ppt" (
   curl -sS "https://github.com/mmmmm4299/CDS/raw/refs/heads/main/PajaPPT.exe" > "%temp%\ppt.exe"
-  start "%temp%\ppt.exe"
+  start "" "%temp%\ppt.exe"
 )
